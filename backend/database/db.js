ca3d3db4fa98fc156c862a937af68a18606e229d
hudson <christhopherrogers@gmail.com>
2025-09-07 22:54:25 -0400

    v 1.1

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 
-const connection = new Sequelize("publi_co", "root", "123456", {
-//const connection = new Sequelize("publi_co", "root", "159753", {
+//const connection = new Sequelize("publi_co", "root", "123456", {
+const connection = new Sequelize("publi_co", "root", "159753", {
   host: "localhost",
   dialect: "mysql"
 });
