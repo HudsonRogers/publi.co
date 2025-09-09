0e03c739362ed7d802e9421c719ce17cdc1053a5
Wilson favini <wilsonfavini@gmail.com>
2025-09-08 21:49:33 -0400

    v1.3 - resolvido problema de conflito no git

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,6 +1,5 @@
 const Sequelize = require("sequelize");
 
-<<<<<<< HEAD
 //configuração para definir a senha do banco de dados
 const os = require('os');
 const userInfo = os.userInfo();
@@ -13,10 +12,6 @@ if (userInfo.username === "wilso") {
 
 const connection = new Sequelize("publi_co", "root", dbPassword, {
 //const connection = new Sequelize("publi_co", "root", "159753", {
-=======
-//const connection = new Sequelize("publi_co", "root", "123456", {
-const connection = new Sequelize("publi_co", "root", "159753", {
->>>>>>> ca3d3db4fa98fc156c862a937af68a18606e229d
   host: "localhost",
   dialect: "mysql"
 });
