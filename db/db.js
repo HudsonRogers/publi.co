a8dcb5af80b3ae5892c01cc8fb539f613a550702
hudson <christhopherrogers@gmail.com>
2025-09-09 20:54:26 -0400

    v1.3.2

--- db/db.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require("sequelize");
-
-const sequelize = new Sequelize("publi_co", "root", "159753", {
-  host: "localhost",
-  dialect: "mysql"
-});
-
-module.exports = sequelize;
